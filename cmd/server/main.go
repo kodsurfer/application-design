8f0eb564ed96b20ecefd32ccaca3c7ae4ec09fb4
kodsurfer <kodsurfer@ya.ru>
2024-06-27 02:16:23 +0300

    update main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,25 @@
-package server
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/kodsurfer/application-design/internal/app"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+	orderService := app.NewOrderService()
+
+	r.Post("/orders", orderService.CreateOrder)
+
+	log.Println("Server listening on localhost:8080")
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatalf("Server failed: %s", err)
+		os.Exit(1)
+	}
+}
